f21e62bb50ff7d02cfcc8a69d4419ac1c862da37
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-10-09 19:00:46 +0100

    Update CombatLogger.php

--- CombatLogger/src/jacknoordhuis/combatlogger/CombatLogger.php
+++ CombatLogger/src/jacknoordhuis/combatlogger/CombatLogger.php
@@ -98,7 +98,7 @@ class CombatLogger extends PluginBase {
 	 *
 	 * @return mixed
 	 */
-	public function getSettingsProperty(string $nested, array $default = []) {
+	public function getSettingsProperty(string $nested, bool $default = []) {
 		return $this->settings->getNested($nested, $default);
 	}
 
